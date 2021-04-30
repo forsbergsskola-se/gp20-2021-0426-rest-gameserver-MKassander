306ef1aeba6dff2b2db449f6424a87aa1bfc3367
Max Kassander <maximilian.kassander@gmail.com>
2021-04-30 17:37:33 +0200

    refactor, exlcude img, readLine

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -16,63 +16,35 @@ namespace TinyBrowser
             try
             {
                 TcpClient client = new TcpClient("acme.com", 80);
-
                 var request = "GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n";
                 var convertedRequest = Encoding.ASCII.GetBytes(request);
 
                 var stream = client.GetStream();
                 stream.Write(convertedRequest, 0,convertedRequest.Length);
-                Console.WriteLine("SENT");
                 var streamReader = new StreamReader(stream);
                 var result = streamReader.ReadToEnd();
                 
-                //Console.WriteLine("RESPONSE: " + result);
                 var title = FindTitle(result, "<title>", "</title>");
                 Console.WriteLine("TITLE: " + title);
-
-
+                
                 string startOfLink = "<a href=";
                 string endOfLink = "</a>";
                 var links = new List<string>();
                 var urls = new List<string>();
                 var displayTexts = new List<string>();
 
-                while (result.Contains(startOfLink))
+                SeparateDesiredStrings(result, startOfLink, endOfLink, links,urls, displayTexts);
+
+                PrintLists(links,displayTexts,urls);
+
+                Console.WriteLine("Enter a number corresponding to the page you want to visit:");
+                var input= Console.ReadLine();
+                
+                if (int.TryParse(input, out int parsedInput) && 
+                    parsedInput >= 0 && parsedInput <= links.Count)
                 {
-                    var indexA = result.IndexOf(startOfLink);
-                    var indexB = indexA;
-                    while (result.Substring(indexB, endOfLink.Length) != endOfLink)
-                        indexB++;
-
-                    indexB += endOfLink.Length;
-
-                    var linkSection = result.Substring(indexA, indexB - indexA);
-                    links.Add(linkSection);
-
-                    string quotation = "\"";
-                    var indexC = linkSection.IndexOf(quotation) +1;
-                    var indexD = indexC;
-                    while (linkSection.Substring(indexD, 1) != quotation)
-                        indexD++;
-
-                    var url = linkSection.Substring(indexC, indexD - indexC);
-                    urls.Add(url);
-
-                    // find display text  => from > => to </a>
-                    var indexE = linkSection.IndexOf(">") +1;
-                    var indexF = indexE;
-                    while (linkSection.Substring(indexF, endOfLink.Length) != endOfLink)
-                        indexF++;
-                    var displayText = linkSection.Substring(indexE, indexF - indexE);
-                    displayTexts.Add(displayText);
-                    
-                    result = result.Remove(indexA, indexB - indexA);
-                }
-
-                for (int i = 0; i < links.Count; i++)
-                {
-                    Console.WriteLine($"{i}: {displayTexts[i]} ({urls[i]})");
-                }
+                    request += urls[parsedInput];
+                }else Console.WriteLine("Invalid input!");
 
                 stream.Close();
             }
@@ -93,6 +65,51 @@ namespace TinyBrowser
             var indexB = result.LastIndexOf(endTerm);
             var title = result.Substring(indexA, indexB - indexA);
             return title;
+        }
+
+        static void SeparateDesiredStrings(string res, string startOf, string endOf, 
+            List<string> linksList, List<string> urlList, List<string> displayList)
+        {
+            while (res.Contains(startOf))
+            {
+                var indexA = res.IndexOf(startOf);
+                var indexB = indexA;
+                while (res.Substring(indexB, endOf.Length) != endOf)
+                    indexB++;
+                indexB += endOf.Length;
+                var linkSection = res.Substring(indexA, indexB - indexA);
+                linksList.Add(linkSection);
+
+                string quotation = "\"";
+                var indexC = linkSection.IndexOf(quotation) +1;
+                var indexD = indexC;
+                while (linkSection.Substring(indexD, 1) != quotation)
+                    indexD++;
+
+                var url = linkSection.Substring(indexC, indexD - indexC);
+                urlList.Add(url);
+
+                var indexE = linkSection.IndexOf(">") +1;
+                var indexF = indexE;
+                while (linkSection.Substring(indexF, endOf.Length) != endOf)
+                    indexF++;
+                var displayText = linkSection.Substring(indexE, indexF - indexE);
+                displayList.Add(displayText);
+                    
+                res = res.Remove(indexA, indexB - indexA);
+            }
+        }
+        
+        static void PrintLists(List<string> linksList, List<string> displayTextList, List<string> urlList)
+        {
+            for (int i = 0; i < linksList.Count; i++)
+            {
+                if (displayTextList[i].StartsWith("<img"))
+                {
+                    Console.WriteLine($"{i}: Image ()");
+                    urlList[i] = "";
+                }else Console.WriteLine($"{i}: {displayTextList[i]} ({urlList[i]})");
+            }
         }
     }
 }
