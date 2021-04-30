2e904c6525867bd43424d9712dee4e86e4aa5c02
Max Kassander <maximilian.kassander@gmail.com>
2021-04-30 11:18:12 +0200

    Revert "some refactoring"
    This reverts commit 3cffbb09a5764bad4fc718104621f93d0d863054.

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -22,12 +22,15 @@ namespace TinyBrowser
 
                 var stream = client.GetStream();
                 stream.Write(convertedRequest, 0,convertedRequest.Length);
+                Console.WriteLine("SENT");
                 var streamReader = new StreamReader(stream);
                 var result = streamReader.ReadToEnd();
                 
+                //Console.WriteLine("RESPONSE: " + result);
                 var title = FindTitle(result, "<title>", "</title>");
                 Console.WriteLine("TITLE: " + title);
-                
+
+
                 string startOfLink = "<a href=";
                 string endOfLink = "</a>";
                 var links = new List<string>();
@@ -37,14 +40,24 @@ namespace TinyBrowser
                 {
                     var indexA = result.IndexOf(startOfLink);
                     var indexB = indexA;
-                    var linkSection = PickOutLinkSection(result, indexA, indexB, endOfLink);
+                    while (result.Substring(indexB, endOfLink.Length) != endOfLink)
+                        indexB++;
+
+                    indexB += endOfLink.Length;
+
+                    var linkSection = result.Substring(indexA, indexB - indexA);
                     links.Add(linkSection);
 
-                    var url = PickOutUrl(linkSection);
+                    string quotation = "\"";
+                    var indexC = linkSection.IndexOf(quotation) +1;
+                    var indexD = indexC;
+                    while (linkSection.Substring(indexD, 1) != quotation)
+                        indexD++;
+
+                    var url = linkSection.Substring(indexC, indexD - indexC);
                     urls.Add(url);
 
                     // find display text  => from > => to </a>
-                    
                     
                     result = result.Remove(indexA, indexB - indexA);
                 }
@@ -74,26 +87,6 @@ namespace TinyBrowser
             var indexB = result.LastIndexOf(endTerm);
             var title = result.Substring(indexA, indexB - indexA);
             return title;
-        }
-
-        static string PickOutLinkSection(string result, int indexA, int indexB, string endOfLink)
-        {
-            while (result.Substring(indexB, endOfLink.Length) != endOfLink)
-                indexB++;
-            indexB += endOfLink.Length;
-            var linkSection = result.Substring(indexA, indexB - indexA);
-            return linkSection;
-        }
-
-        static string PickOutUrl(string linkSection)
-        {
-            string quotation = "\"";
-            var indexC = linkSection.IndexOf(quotation) +1;
-            var indexD = indexC;
-            while (linkSection.Substring(indexD, 1) != quotation)
-                indexD++;
-            var url = linkSection.Substring(indexC, indexD - indexC);
-            return url;
         }
     }
 }
