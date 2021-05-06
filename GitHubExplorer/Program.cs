2e37d1fbdadb140b9afbb8641e04bb92e5d28d0e
Max Kassander <maximilian.kassander@gmail.com>
2021-05-06 16:38:14 +0200

    Progress?

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -12,15 +12,16 @@ namespace GitHubExplorer
         {
             while (true)
             {
-                var input = Console.ReadLine();
-                
-                
-                
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 10000);
                 HttpClient client = new HttpClient();
-                //client.DefaultRequestHeaders.Add();
-
+                client.DefaultRequestHeaders.Add("GitHubExplorer", "1.0");
                 
+                Console.WriteLine("Enter username");
+                var input = Console.ReadLine();
+                var baseUrl = "https//api.github.com/users/";
+                var requestUrl = baseUrl + input;
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                client.Send(requestMessage);
+
 
                 HttpResponseMessage responseMessage = new HttpResponseMessage();
                 var stream = responseMessage.Content.ReadAsStream();
