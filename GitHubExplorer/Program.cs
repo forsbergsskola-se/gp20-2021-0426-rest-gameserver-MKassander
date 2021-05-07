a64852fb9fb66330066ac544bf811edc53efa17c
Max Kassander <maximilian.kassander@gmail.com>
2021-05-07 11:34:54 +0200

    token & gitIgnore

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text.Json;
 
 namespace GitHubExplorer
 {
@@ -10,6 +9,7 @@ namespace GitHubExplorer
     {
         static void Main(string[] args)
         {
+            var token = Env.token; //Class Env need to be created with your token in string "token" 
             while (true)
             {
                 HttpClient client = new HttpClient();
