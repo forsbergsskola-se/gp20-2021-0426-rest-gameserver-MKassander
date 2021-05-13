2445a2b0d230c0c95be0e4e1ad7e03272c33efec
Max Kassander <maximilian.kassander@gmail.com>
2021-05-13 23:56:03 +0200

    Part2 progress, close to done

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -3,38 +3,55 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
     class Program
     {
+        static string separatorString = "*******";
         static void Main(string[] args)
         {
-            var token = Env.token; //Class Env need to be created with your token in string "token" 
             while (true)
             {
                 HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", token);
+                var token = Env.token; //Class Env need to be created with your token in string "token" 
+                
+                client.BaseAddress = new Uri("https://api.github.com/users/");
+                client.DefaultRequestHeaders.UserAgent.Add
+                    (new ProductInfoHeaderValue("GithubExplorer", "1.0"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
+                    ("token", token);
                 
                 Console.WriteLine("Enter username");
                 var input = Console.ReadLine();
-                client.BaseAddress = new Uri("https://api.github.com/users/");// 
-                //var requestUrl = baseUrl + input;
-                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, input);
-                client.Send(requestMessage);
                 
-                Console.WriteLine("Sending...");
-                HttpResponseMessage responseMessage = new HttpResponseMessage();
-                var stream = responseMessage.Content.ReadAsStream();
-
-                Console.WriteLine("Received response...");
-                StreamReader streamReader = new StreamReader(stream);
-                var stringFromStream = streamReader.ReadToEnd();
-                Console.WriteLine(stringFromStream);
-                Console.WriteLine("End");
-
+                var task = ToAndFromGit(input, client);
+                task.Wait();
+                
                 client.Dispose();
             }
+        }
+
+        static async Task ToAndFromGit(string input, HttpClient client)
+        {
+            var response = await client.GetAsync(input);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+
+            Separator();
+            Console.WriteLine(response);
+            Separator();
+            Console.WriteLine(body);
+            Separator();
+        }
+
+        static void Separator()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(separatorString);
+            Console.ResetColor();
         }
     }
 }
