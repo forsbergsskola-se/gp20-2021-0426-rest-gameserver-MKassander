faa0845c49019c6b44c370a084c96dc00417ee0e
Max Kassander <maximilian.kassander@gmail.com>
2021-05-14 16:13:19 +0200

    Adds UserResponse /w fields & print method

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
@@ -10,7 +9,26 @@ namespace GitHubExplorer
 {
     public class UserResponse
     {
-        
+        public string name { get; set; }
+        public string company { get; set; }
+        public string location { get; set; }
+        public string email { get; set; }
+        public string bio { get; set; }
+        public int public_repos { get; set; }
+        public int private_Repos { get; set; }
+
+        public void PrintFields()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Company: " + company);
+            Console.WriteLine("Bio: " + bio);
+            Console.WriteLine("Location: " + location);
+            Console.WriteLine("Private repos: " + private_Repos);
+            Console.WriteLine("public repos: " + public_repos);
+            Console.WriteLine("Email: " + email);
+            Console.ResetColor();
+        }
     }
     class Program
     {
@@ -19,27 +37,27 @@ namespace GitHubExplorer
         {
             while (true)
             {
-                HttpClient client = new HttpClient();
-                var token = Env.token; //Class Env need to be created with your token in string "token" 
-                
-                client.BaseAddress = new Uri("https://api.github.com/users/");
-                client.DefaultRequestHeaders.UserAgent.Add
-                    (new ProductInfoHeaderValue("GithubExplorer", "1.0"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
-                    ("token", token);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var client = SetUp();
                 
                 Console.WriteLine("Enter username");
                 var input = Console.ReadLine();
                 
-                var task = ToAndFromGit(input, client);
+                var task = UserFromGit(input, client);
                 task.Wait();
                 
+                Console.WriteLine("Enter \"1\" to go to repositories");
+                Console.WriteLine("Enter any other input to exit");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    
+                }
+                Console.WriteLine("Closing client.");
                 client.Dispose();
             }
         }
 
-        static async Task ToAndFromGit(string input, HttpClient client)
+        static async Task UserFromGit(string input, HttpClient client)
         {
             var response = await client.GetAsync(input);
             response.EnsureSuccessStatusCode();
@@ -49,12 +67,15 @@ namespace GitHubExplorer
             var responseString = await streamReader.ReadToEndAsync();
 
             var userResponse = JsonSerializer.Deserialize<UserResponse>(responseString);
-            //var user = new UserResponse(userResponse);
+            
 
+            
             Separator();
             Console.WriteLine(response);
             Separator();
             Console.WriteLine(responseString);
+            Separator();
+            userResponse.PrintFields();
             Separator();
         }
 
@@ -64,6 +85,20 @@ namespace GitHubExplorer
             for (int i = 0; i < 3; i++)
                 Console.WriteLine(separatorString);
             Console.ResetColor();
+        }
+
+        static HttpClient SetUp()
+        {
+            HttpClient client = new HttpClient();
+            var token = Env.token; //Class Env need to be created with your token in string "token" 
+                
+            client.BaseAddress = new Uri("https://api.github.com/users/");
+            client.DefaultRequestHeaders.UserAgent.Add
+                (new ProductInfoHeaderValue("GithubExplorer", "1.0"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
+                ("token", token);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
         }
     }
 }
