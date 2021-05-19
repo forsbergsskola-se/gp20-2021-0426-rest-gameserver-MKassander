793f9ee922db17f1109958b198c93f0af070dcfe
Max Kassander <maximilian.kassander@gmail.com>
2021-05-19 18:36:48 +0200

    LameScooter part 1-2.5

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace LameScooter
 {
+    public interface ILameScooterRental
+    {
+        Task<int> GetScooterCountInStation(string stationName);
+    }
+
+    public class OfflineLameScooterRental: ILameScooterRental
+    {
+        public async Task<int> GetScooterCountInStation(string stationName)
+        {
+            int result = 0;
+            var file = await File.ReadAllTextAsync("scooters.json");
+            var json = JsonSerializer.Deserialize<LameScooterStationList>(file);
+            foreach (var station in json.ScooterStations)
+                if (stationName == station.name)
+                    result = station.bikesAvailable;
+
+            return result;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OfflineLameScooterRental rental = new OfflineLameScooterRental();
+
+            var count = await rental.GetScooterCountInStation(args[0]);
+            Console.WriteLine("Count: " + count);
         }
+    }
+
+    public class LameScooterStation
+    {
+        public string name { get; set; }
+        public int bikesAvailable { get; set; }
+    }
+    
+    public class LameScooterStationList
+    {
+        public List<LameScooterStation> ScooterStations;
     }
 }
