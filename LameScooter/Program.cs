653dc495da77e9c82d3b73bcfd83c586cadf3433
Max Kassander <maximilian.kassander@gmail.com>
2021-05-21 12:20:53 +0200

    part 7

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -8,13 +8,40 @@ namespace LameScooter
     {
         static async Task Main(string[] args)
         {
-            var rental = new DeprecatedLameScooterRental();
-
             if (args[0].Any(char.IsDigit))
                 throw  new ArgumentException("Invalid Argument");
-            
-            var count = await rental.GetScooterCountInStation(args[0]);
+
+            var count = await ReadArgsGetCount(args);
             Console.WriteLine(count);
+        }
+
+        static async Task<int> ReadArgsGetCount(string[] argument)
+        {
+            var deprecatedRental = new DeprecatedLameScooterRental();
+            var offlineRental = new OfflineLameScooterRental();
+            int count;
+            if (argument[0].Contains(" "))
+            {
+                if (argument[0].Substring(argument[0].IndexOf(" ") +1 ) == "offline")
+                {
+                    count = await offlineRental.GetScooterCountInStation
+                        (argument[0].Substring(0,argument[0].IndexOf(" ")));
+                    return count;
+
+                }
+                if (argument[0].Substring(argument[0].IndexOf(" ") +1 ) == "deprecated")
+                {
+                    count = await deprecatedRental.GetScooterCountInStation
+                        (argument[0].Substring(0,argument[0].IndexOf(" ")));
+                    return count;
+                }
+            }
+            else
+            {
+                count = await offlineRental.GetScooterCountInStation(argument[0]);
+                return count;
+            }
+            throw new Exception();
         }
     }
 }
