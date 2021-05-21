cf9f9f446f8052da3c9c41b1dcc7440755cb35bb
Max Kassander <maximilian.kassander@gmail.com>
2021-05-21 16:08:09 +0200

    slight adjustments

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -17,44 +17,38 @@ namespace LameScooter
 
         static async Task<int> ReadArgsGetCount(string[] argument)
         {
-            var deprecatedRental = new DeprecatedLameScooterRental();
-            var offlineRental = new OfflineLameScooterRental();
-            var realtimeRental = new RealTimeLameScooterRental();
-            int count;
             if (argument.Length > 1)
             {
                 if (argument[1] == "offline")
                 {
+                    var offlineRental = new OfflineLameScooterRental();
                     Print.WritelineWithColor("Using offlineRental...", ConsoleColor.Blue);
-                    count = await offlineRental.GetScooterCountInStation(argument[0]);
-                    return count;
+                    return await offlineRental.GetScooterCountInStation(argument[0]);
 
                 }
                 if (argument[1] == "deprecated")
                 {
+                    var deprecatedRental = new DeprecatedLameScooterRental();
                     Print.WritelineWithColor("Using deprecatedRental...", ConsoleColor.Blue);
-                    count = await deprecatedRental.GetScooterCountInStation(argument[0]);
-                    return count;
+                    return await deprecatedRental.GetScooterCountInStation(argument[0]);
                 }
                 if (argument[1] == "realtime")
                 {
+                    var realtimeRental = new RealTimeLameScooterRental();
                     Print.WritelineWithColor("Using realtimeRental...", ConsoleColor.Blue);
-                    count = await realtimeRental.GetScooterCountInStation(argument[0]);
-                    return count;
+                    return await realtimeRental.GetScooterCountInStation(argument[0]);
                 }
             }
             else
             {
-                Console.WriteLine("else");
-                count = await offlineRental.GetScooterCountInStation(argument[0]);
-                return count;
+                Print.WritelineWithColor("No second argument for method entered!", ConsoleColor.Blue);
+                Print.WritelineWithColor("Using offlineRental...", ConsoleColor.Blue);
+                var offlineRental = new OfflineLameScooterRental();
+                return await offlineRental.GetScooterCountInStation(argument[0]);
             }
             throw new Exception();
         }
-        
-        
     }
-
     static class Print
     {
         public static void WritelineWithColor(string message, ConsoleColor color)
